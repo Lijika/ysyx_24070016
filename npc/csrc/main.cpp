0bb71b45c01e94af41fa27c7dcc69d6e166193f3
lhj <1036822390@qq.com>
2024-03-15 15:03:50 +0800

    NJU-ProjectN/am-kernels ics2021 initialized without tracing
    NJU-ProjectN/am-kernels bb725d6f8223dd7de831c3b692e8c4531e9d01af kernels,demo,donut: only clear screen before drawing

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <verilated.h>
+#include "Vtop.h"
+#include "verilated_vcd_c.h"
+#include <nvboard.h>
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(Vtop* top);
+
+int main(int argc, char** argv) {
+	VerilatedContext* contextp = new VerilatedContext;
+	contextp->commandArgs(argc,argv);
+	Vtop* top = new Vtop{contextp};
+
+	Verilated::traceEverOn(true);
+	VerilatedVcdC* tfp = new VerilatedVcdC;
+	top->trace(tfp, 0);
+	tfp->open("wave.vcd");
+
+	nvboard_bind_all_pins(&dut);
+	nvboard_init();
+	
+
+	while (!contextp->gotFinish())
+	{
+		int a = rand() & 1;
+		int b = rand() & 1;
+		top->a = a;
+		top->b = b;
+
+		top->eval();
+		tfp->dump(contextp->time());
+		contextp->timeInc(1);
+		printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+		assert(top->f == (a ^ b));
+		while (1) 
+		{
+			dut.eval();
+			nvboard_update();
+		}
+	}
+
+	nvboard_quit();
+	tfp->close();
 }
