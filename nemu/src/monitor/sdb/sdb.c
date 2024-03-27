b62db20969176392872bbdb2ab9606775b9dbab3
tracer-ysyx <tracer@ysyx.org>
2024-03-27 18:37:34 +0800

    > compile NEMU ysyx_22040000 李航江 Linux LAPTOP-BJ3F4QK8 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:37:34 up  7:35,  1 user,  load average: 0.15, 0.17, 0.11

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -68,7 +68,9 @@ static int cmd_info(char *args) {
 	int num_info_args = strlen(info_args);
 	int i;
 	for ( i = 0; i < num_info_args; i++) {
-		if (strcmp(args, &info_args[i]) == 0) {
+		if (args == NULL) { 
+			printf("info:Missing args. \n'info r'-prints register status. \n'info w'-prints watchpoint information.");
+		} else if (strcmp(args, &info_args[i]) == 0) {
 			switch (i) {
 			case 0: isa_reg_display();	//info r
 
