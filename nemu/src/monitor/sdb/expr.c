4033e6849146be98835b0fc2c4d5b13da0b26d40
tracer-ysyx <tracer@ysyx.org>
2024-03-31 17:12:42 +0800

    > compile NEMU ysyx_22040000 李航江 Linux LAPTOP-BJ3F4QK8 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:12:42 up  2:22,  1 user,  load average: 0.15, 0.16, 0.11

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -216,8 +216,8 @@ word_t eval(int p, int q) {
   }
   else {
     int op = search_main_token_position(p, q);
-    unsigned int val1 = eval(p, op - 1);
-    unsigned int val2 = eval(op + 1, q);
+    int val1 = eval(p, op - 1);
+    int val2 = eval(op + 1, q);
 
     switch (tokens[op].type) {
       case '+': return val1 + val2;
