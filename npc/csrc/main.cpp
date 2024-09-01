1141b56348d47caf3574d51a2bf178586cc424a9
tracer-ysyx <tracer@ysyx.org>
2024-09-01 18:35:05 +0800

    > compile NEMU ysyx_24070016 李航江 Linux LAPTOP-BJ3F4QK8 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:35:05 up 16 min,  1 user,  load average: 0.00, 0.06, 0.09

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -6,11 +6,38 @@
 #include "verilated_vcd_c.h"
 // #include <nvboard.h>
 
-// static TOP_NAME dut;
+#define PMEMSIZE 0x8000000
+#define PMEMBASE 0x80000000
 
+// static TOP_NAME dut;
 // void nvboard_bind_all_pins(Vtop* top);
 
+uint8_t pmem[PMEMSIZE] __attribute((aligned(4096))) = {};
+
+void init_pmem(uint8_t *pmem) {
+	const uint32_t img [] = {
+		0x01000093,		//li	ra,16
+		0x01008113,		//addi	sp,ra,16
+		0x01f08113		//addi	sp,ra,31
+	};
+
+	memcpy(pmem, img, sizeof(img));
+}
+
+uint32_t pmem_read(uint32_t paddr, int len) {
+	bool in_pmem = paddr - PMEMBASE < PMEMSIZE;
+	uint8_t* host_addr = paddr - PMEMBASE + pmem;
+	
+	if(in_pmem) return *(uint32_t *)host_addr;
+
+	printf("Memory access address out of bounds!");
+	assert(0);
+}
+
 int main(int argc, char** argv) {
+	// nvboard_bind_all_pins(&dut);
+	// nvboard_init();
+
 	VerilatedContext* contextp = new VerilatedContext;
 	contextp->commandArgs(argc,argv);
 	Vysyx_24070016_top* top = new Vysyx_24070016_top{contextp};
@@ -19,15 +46,25 @@ int main(int argc, char** argv) {
 	VerilatedVcdC* tfp = new VerilatedVcdC;
 	top->trace(tfp, 1);
 	tfp->open("wave.fst");
-
-	// nvboard_bind_all_pins(&dut);
-	// nvboard_init();
-	
 	vluint64_t sim_time = contextp->time();
+	
+	init_pmem(pmem);
 
-	while (!contextp->gotFinish())
-	{
 
+	while (1) {
+		if (sim_time >= 3) {
+			break;
+		}
+
+		top->clk = 0;
+		top->rst = 0;
+		top->
+		top->eval();
+		contextp->timeInc(1);
+
+		top->clk = 1;
+		top->eval();
+		contextp->timeInc(1);
 	}
 
 	// nvboard_quit();
