812474e35c3563ba17e7c413315bb689625695cf
tracer-ysyx <tracer@ysyx.org>
2024-09-01 16:39:29 +0800

    > compile NEMU ysyx_24070016 李航江 Linux LAPTOP-BJ3F4QK8 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:39:29 up  7:52,  1 user,  load average: 0.08, 0.13, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -2,47 +2,39 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <verilated.h>
-#include "Vtop.h"
+#include "Vysyx_24070016_top.h"
 #include "verilated_vcd_c.h"
-#include <nvboard.h>
+// #include <nvboard.h>
 
-static TOP_NAME dut;
+// static TOP_NAME dut;
 
-void nvboard_bind_all_pins(Vtop* top);
+// void nvboard_bind_all_pins(Vtop* top);
 
 int main(int argc, char** argv) {
 	VerilatedContext* contextp = new VerilatedContext;
 	contextp->commandArgs(argc,argv);
-	Vtop* top = new Vtop{contextp};
+	Vysyx_24070016_top* top = new Vysyx_24070016_top{contextp};
 
 	Verilated::traceEverOn(true);
 	VerilatedVcdC* tfp = new VerilatedVcdC;
-	top->trace(tfp, 0);
-	tfp->open("wave.vcd");
+	top->trace(tfp, 1);
+	tfp->open("wave.fst");
 
-	nvboard_bind_all_pins(&dut);
-	nvboard_init();
+	// nvboard_bind_all_pins(&dut);
+	// nvboard_init();
 	
+	vluint64_t sim_time = contextp->time();
 
 	while (!contextp->gotFinish())
 	{
-		int a = rand() & 1;
-		int b = rand() & 1;
-		top->a = a;
-		top->b = b;
 
-		top->eval();
-		tfp->dump(contextp->time());
-		contextp->timeInc(1);
-		printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-		assert(top->f == (a ^ b));
-		while (1) 
-		{
-			dut.eval();
-			nvboard_update();
-		}
 	}
 
-	nvboard_quit();
-	tfp->close();
+	// nvboard_quit();
+	if (tfp) {
+		tfp->close();
+		delete tfp;
+	}
+	delete top;
+	delete contextp;
 }
