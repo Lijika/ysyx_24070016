73b4e59c7cb043c50952983cf8792ffe8f3b43ce
tracer-ysyx <tracer@ysyx.org>
2024-09-06 20:38:57 +0800

    > sim RTL ysyx_24070016 李航江 Linux LAPTOP-BJ3F4QK8 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:38:57 up  2:19,  1 user,  load average: 0.25, 0.15, 0.08

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -110,9 +110,10 @@ int main(int argc, char** argv) {
 	num_inst = init_pmem(pmem);
 
 	top->rst = 1;   // 复位信号设为高电平
-	top->clk = 0;   // 时钟低电平
+	top->clk = 1;   // 时钟低电平
 	step_and_dump_wave();  // 仿真一步
-	top->clk ^= 1;   // 时钟高电平，触发复位
+	top->clk ^= 0;   // 时钟高电平，触发复位
+	top->inst_mem_rdata = pmem_read_if((int)top->inst_mem_addr);
 	step_and_dump_wave();  // 仿真一步
 	// top->inst_mem_rdata = pmem_read_if((int)top->inst_mem_addr);
 	// printf("pc = %#x, inst = %#x\n", top->inst_mem_addr, top->inst_mem_rdata);
@@ -121,9 +122,9 @@ int main(int argc, char** argv) {
 	while (1) {
 		// if(sim_cycle == 10) break;
 		top->rst = 0;   // 复位信号设为低电平
-		top->clk = 0;   // 时钟低电平
+		top->clk = 1;   // 时钟低电平
 		step_and_dump_wave();  // 仿真一步
-		top->clk = 1;   // 时钟高电平，触发复位
+		top->clk = 0;   // 时钟高电平，触发复位
 		top->inst_mem_rdata = pmem_read_if((int)top->inst_mem_addr);
 		step_and_dump_wave();  // 仿真一步
 		printf("pc = %#x, inst = %#x\n", top->inst_mem_addr, top->inst_mem_rdata);
