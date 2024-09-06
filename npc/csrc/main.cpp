5dc8dc867456310a67da6e47ca67e517cda8e29b
tracer-ysyx <tracer@ysyx.org>
2024-09-06 21:30:14 +0800

    > sim RTL ysyx_24070016 李航江 Linux LAPTOP-BJ3F4QK8 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:30:14 up  3:10,  1 user,  load average: 0.25, 0.23, 0.14

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -110,29 +110,37 @@ int main(int argc, char** argv) {
 	sim_cycle = contextp->time();
 	num_inst = init_pmem(pmem);
 
-	top->rst = 1;   // 复位信号设为高电平
-	top->clk = 0;   // 时钟低电平
-	step_and_dump_wave();  // 仿真一步
-	top->clk ^= 1;   // 时钟高电平，触发复位
-	step_and_dump_wave();  // 仿真一步
-	top->inst_mem_rdata = pmem_read_if((int)top->inst_mem_addr);
-	// printf("pc = %#x, inst = %#x\n", top->inst_mem_addr, top->inst_mem_rdata);
-	sim_cycle++;
+	// top->rst = 1;   // 复位信号设为高电平
+	// top->clk = 0;   // 时钟低电平
+	// step_and_dump_wave();  // 仿真一步
+	// top->clk ^= 1;   // 时钟高电平，触发复位
+	// step_and_dump_wave();  // 仿真一步
+	// top->inst_mem_rdata = pmem_read_if((int)top->inst_mem_addr);
+	// // printf("pc = %#x, inst = %#x\n", top->inst_mem_addr, top->inst_mem_rdata);
+	// sim_cycle++;
 
-	while (1) {
-		// if(sim_cycle == 10) break;
-		top->rst = 0;   // 复位信号设为低电平
-		top->clk = 0;   // 时钟低电平
-		step_and_dump_wave();  // 仿真一步
-		top->clk = 1;   // 时钟高电平，触发复位
-		step_and_dump_wave();  // 仿真一步
-		top->inst_mem_rdata = pmem_read_if((int)top->inst_mem_addr);
-		printf("pc = %#x, inst = %#x\n", top->inst_mem_addr, top->inst_mem_rdata);
-		sim_cycle++;
+	// while (1) {
+	// 	// if(sim_cycle == 10) break;
+	// 	top->rst = 0;   // 复位信号设为低电平
+	// 	top->clk = 0;   // 时钟低电平
+	// 	step_and_dump_wave();  // 仿真一步
+	// 	top->clk = 1;   // 时钟高电平，触发复位
+	// 	step_and_dump_wave();  // 仿真一步
+	// 	top->inst_mem_rdata = pmem_read_if((int)top->inst_mem_addr);
+	// 	printf("pc = %#x, inst = %#x\n", top->inst_mem_addr, top->inst_mem_rdata);
+	// 	sim_cycle++;
+	// }
+
+	int half_clock_period = 5; //1周期10个步长
+	top->rst = 1;
+
+	while(1) {
+		if(contextp->time() % half_clock_period == 0) top->clk ^= 1;
+		if(contextp->time() >= 10) top->rst = 0;
+		step_and_dump_wave();
+		
+		if(top->clk == 1) top->inst_mem_rdata = pmem_read_if((int)top->inst_mem_addr);
 	}
-
-	top->clk = 0;   // 时钟低电平
-	step_and_dump_wave();  // 仿真一步
 	
 	// nvboard_quit();
 	if (tfp) {
