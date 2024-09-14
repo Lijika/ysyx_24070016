8b16ec87bc52ac7ea04f2ae5fae6012490946bf8
tracer-ysyx <tracer@ysyx.org>
2024-09-14 20:58:12 +0800

    > compile NEMU ysyx_24070016 李航江 Linux LAPTOP-BJ3F4QK8 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:58:12 up  1:37,  1 user,  load average: 0.01, 0.06, 0.08

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -9,7 +9,14 @@ size_t strlen(const char *s) {
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char *orign_dst = dst;
+  while(*src != '\0') {
+	*dst = *src;
+	dst++;
+	src++;
+  }
+  *dst = '\0';
+  return orign_dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,11 +24,28 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *orign_dst = dst;
+  while (*dst != '\0') {
+	dst++;
+  }
+  while(*src != '\0') {
+	*dst = *src;
+	dst++;
+	src++;
+  }
+  *dst = '\0';
+  return orign_dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  while((*s1 != '\0') && (*s2 != '\0') && (*s1 == *s2)) {
+	s1++;
+	s2++;
+  }
+  unsigned char ch1 = *s1;
+  unsigned char ch2 = *s2;
+
+  return ch1 - ch2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,7 +53,14 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  int i;
+  unsigned char ch = c;
+  unsigned char *dst = s;
+  for(i = 0; i < 0; i++) {
+    *dst = ch;
+    dst++;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -41,7 +72,18 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  int i;
+  const unsigned char *ch1 = s1;
+  const unsigned char *ch2 = s2;
+  for(i = 0; i < n; i++) {
+	if(*ch1 == *ch2) {
+	  ch1++;
+	  ch2++;
+	  continue;
+	}
+	return *ch1 - *ch2;
+  }
+  return 0;
 }
 
 #endif
