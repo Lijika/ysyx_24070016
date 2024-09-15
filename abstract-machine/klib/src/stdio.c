7b5e71b8254d4c7e8b5dfeebb362232f5e389aed
tracer-ysyx <tracer@ysyx.org>
2024-09-15 14:55:05 +0800

    > compile NEMU ysyx_24070016 李航江 Linux LAPTOP-BJ3F4QK8 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:55:05 up  3:28,  1 user,  load average: 0.22, 0.16, 0.10

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,34 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+int int2str(int src, char *str) {
+  int is_neg = 0;
+  int i = 0;
+  if(src < 0) {
+    src *= -1;
+    is_neg = 1;
+  }
+
+  while(src) {
+    str[i++] = (src % 10) + '\0';
+    src /= 10;
+  }
+
+  if(is_neg) { str[i++] = '-'; }
+  str[i] = '\0';
+
+  int lchar = 0;
+  int rchar = i - 1;
+  while(lchar < rchar) {
+    char temp = str[lchar];
+    str[lchar] = str[rchar];
+    str[rchar] = temp;
+    lchar++;
+    rchar--;
+  }
+  return i;
+}
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -14,7 +42,40 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  int ch_num = 0;
+  va_list ap;
+  
+  va_start(ap, fmt);
+  while(*fmt) {
+    if(*fmt != '%') {
+      *(out++) = *(fmt++);
+      ch_num++;
+      continue;
+    }
+    
+    switch(*(fmt+1)) {
+      case 's': {
+        char *str = va_arg(ap, char*);
+        while(*str) { *(out++) = *(str++); ch_num++; }
+        break;
+      }
+      case 'd': {
+        int num = va_arg(ap, int);
+        int d_ch_num = int2str(num, out);
+        out += d_ch_num;
+        ch_num += d_ch_num;
+        break;
+      }
+      default: {
+        *(out++) = *fmt;
+        ch_num++;
+        break;
+      }
+    }
+  }
+  va_end(ap);
+
+  return ch_num;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
