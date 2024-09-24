b8515270c810630a6403e82bc4fdf19fb169ff48
tracer-ysyx <tracer@ysyx.org>
2024-09-24 21:33:31 +0800

    > compile NEMU ysyx_24070016 李航江 Linux LAPTOP-BJ3F4QK8 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:33:31 up  2:30,  1 user,  load average: 0.24, 0.20, 0.14

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -42,10 +42,8 @@ int strcmp(const char *s1, const char *s2) {
 	s1++;
 	s2++;
   }
-  unsigned char ch1 = *s1;
-  unsigned char ch2 = *s2;
 
-  return ch1 - ch2;
+  return *(unsigned char*)s1 - *(unsigned char*)s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -53,12 +51,9 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  int i;
-  unsigned char ch = c;
-  unsigned char *dst = s;
-  for(i = 0; i < 0; i++) {
-    *dst = ch;
-    dst++;
+  unsigned char *dst = (unsigned char *)s;
+  for(size_t i = 0; i < n; i++) {
+    dst[i] = (unsigned char)c;
   }
   return s;
 }
@@ -72,16 +67,12 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  int i;
-  const unsigned char *ch1 = s1;
-  const unsigned char *ch2 = s2;
-  for(i = 0; i < n; i++) {
-	if(*ch1 == *ch2) {
-	  ch1++;
-	  ch2++;
-	  continue;
+  const unsigned char *ch1 = (const unsigned char *)s1;
+  const unsigned char *ch2 = (const unsigned char *)s2;
+  for(size_t i = 1; i < n; i++) {
+	if(ch1[i] != ch2[i]) {
+	  return ch1[i] - ch2[i];
 	}
-	return *ch1 - *ch2;
   }
   return 0;
 }
