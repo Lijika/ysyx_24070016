adda959c28f052d3ef900bfd9a8f26c5347414b9
tracer-ysyx <tracer@ysyx.org>
2024-09-24 20:24:55 +0800

    > compile NEMU ysyx_24070016 李航江 Linux LAPTOP-BJ3F4QK8 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:24:55 up  1:21,  1 user,  load average: 0.29, 0.13, 0.09

--- nemu/src/monitor/sdb/iringbuf.c
+++ nemu/src/monitor/sdb/iringbuf.c
@@ -39,13 +39,13 @@ void add_instruction(Decode *s) {
   p[0] = '\0'; // the upstream llvm does not support loongarch32r
 #endif
 
-  int i;
+//   int i;
   printf("%d", (int)(p - rb->buffer[rb->wr_ptr]));
 //   p = rb->buffer[rb->wr_ptr] + 30;
-  uint8_t *inst = (uint8_t *)&s->isa.inst.val;
-  for(i = ilen - 1; i >= 0; i--) {
-	p += snprintf(p, 4, " %02x", inst[i]);
-  }
+//   uint8_t *inst = (uint8_t *)&s->isa.inst.val;
+//   for(i = ilen - 1; i >= 0; i--) {
+// 	p += snprintf(p, 4, " %02x", inst[i]);
+//   }
 
   rb->wr_ptr = (rb->wr_ptr + 1) % rb->length;
 }
