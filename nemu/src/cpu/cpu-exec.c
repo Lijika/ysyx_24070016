b2ae428a1747134e2dcaf52f441fccd36766818c
tracer-ysyx <tracer@ysyx.org>
2024-09-25 19:53:30 +0800

    > compile NEMU ysyx_24070016 李航江 Linux LAPTOP-BJ3F4QK8 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:53:30 up  5:15,  1 user,  load average: 0.10, 0.11, 0.09

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -63,7 +63,6 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 static void exec_once(Decode *s, vaddr_t pc) {
   s->pc = pc;
   s->snpc = pc;
-  printf("in exec_once m = %d\n", m->is_access_mem);
   isa_exec_once(s);
   cpu.pc = s->dnpc;
 #ifdef CONFIG_ITRACE
