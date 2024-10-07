93d09575a462df68f1ae25c0b65ea667769733d7
tracer-ysyx <tracer@ysyx.org>
2024-10-07 20:50:09 +0800

    > compile NEMU ysyx_24070016 李航江 Linux LAPTOP-BJ3F4QK8 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:50:09 up 56 min,  1 user,  load average: 0.59, 0.24, 0.13

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -85,7 +85,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
-	  case 'e': elf_file = optarg; break;
+	  case 'e': printf("///%s///", optarg); elf_file = optarg; init_elf(elf_file); break;
       case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
@@ -127,9 +127,6 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Initialize differential testing. */
   init_difftest(diff_so_file, img_size, difftest_port);
-
-  /* Initialize the elf file. */
-  init_elf(elf_file);
 
   /* Initialize the simple debugger. */
   init_sdb();
