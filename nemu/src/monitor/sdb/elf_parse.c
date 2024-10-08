e4bd675f81d02ff63cd232984d6bde773dcf3d82
tracer-ysyx <tracer@ysyx.org>
2024-10-08 20:17:29 +0800

    > compile NEMU ysyx_24070016 李航江 Linux LAPTOP-BJ3F4QK8 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:17:29 up 40 min,  1 user,  load average: 0.05, 0.09, 0.10

--- nemu/src/monitor/sdb/elf_parse.c
+++ nemu/src/monitor/sdb/elf_parse.c
@@ -39,7 +39,7 @@ void init_elf(char *elf_file) {
 	Elf32_Shdr *shdr = (Elf32_Shdr *)(elf_buf + ehdr->e_shoff);
 
 
-	printf("///%s ///\n", (char *)(shdr + ehdr->e_shstrndx) + 22);
+	printf("///%s ///\n", (char *)(shdr + ehdr->e_shstrndx) + 50);
 	assert(0);
 
 
