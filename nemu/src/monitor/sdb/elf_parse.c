1861f81ebf3e33bd9460b938f688dd94f247c8f6
tracer-ysyx <tracer@ysyx.org>
2024-10-08 19:52:19 +0800

    > compile NEMU ysyx_24070016 李航江 Linux LAPTOP-BJ3F4QK8 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:52:19 up 15 min,  1 user,  load average: 0.06, 0.15, 0.16

--- nemu/src/monitor/sdb/elf_parse.c
+++ nemu/src/monitor/sdb/elf_parse.c
@@ -1,36 +1,44 @@
 #include <elf.h>
 #include <isa.h>
 
-void init_elf(char *elf_file) {
-	FILE *fd = fopen(elf_file, "r");
-	if(fd == NULL) {
+char *read_elf_file(char *elf_file) {
+	FILE *fp = fopen(elf_file, "r");
+	if(fp == NULL) {
 		printf("fail to open the elf file.\n");
 		assert(0);
 	}
-
-	Elf32_Ehdr ehdr;
+	fseek(fp, 0, SEEK_END);
+	int elf_size = ftell(fp);
+	char *elf_buf = (char *)malloc(elf_size);
+	
 	int a;
-
-	a = fread(&ehdr, sizeof(Elf32_Ehdr), 1, fd);
+	a = fread(elf_buf, 1, elf_size, fp);
 	if(a == 0) {
-		printf("fail to read elf head.\n");
+		printf("fail to read elf file\n");
 		assert(0);
 	}
+
+	return elf_buf;
+}
+
+void init_elf(char *elf_file) {
+	char *elf_buf = read_elf_file(elf_file);
+
+	Elf32_Ehdr *ehdr = (Elf32_Ehdr *)elf_buf;
 	// printf("\n//%d//\n", ehdr.e_shstrndx);
 
-	if((ehdr.e_ident[0] != 0x7F) ||
-		ehdr.e_ident[1] != 'E' ||
-		ehdr.e_ident[2] != 'L' ||
-		ehdr.e_ident[3] != 'F') {
+	if((ehdr->e_ident[0] != 0x7F) ||
+		ehdr->e_ident[1] != 'E' ||
+		ehdr->e_ident[2] != 'L' ||
+		ehdr->e_ident[3] != 'F') {
 		printf("the file is not ELF.\n");
 		assert(0);
 	}
 
-	Elf32_Shdr *shdr = (Elf32_Shdr*)malloc(sizeof(Elf32_Shdr) * ehdr.e_shnum);
-	fseek(fd, ehdr.e_shoff, SEEK_SET);
-	a = fread(shdr, sizeof(Elf32_Shdr) * ehdr.e_shnum, 1, fd);
+	Elf32_Shdr *shdr = (Elf32_Shdr *)(ehdr + ehdr->e_shoff);
 
-	printf("///%s ///\n", (char *)(shdr + ehdr.e_shstrndx + shdr->sh_name));
+
+	printf("///%s ///\n", (char *)(shdr + ehdr->e_shstrndx + shdr->sh_name));
 	assert(0);
 
 
