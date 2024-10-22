4603d4c7d687f588cd30394bfc9499da91fa2e04
tracer-ysyx <tracer@ysyx.org>
2024-10-22 21:04:00 +0800

    > compile NEMU ysyx_24070016 李航江 Linux LAPTOP-BJ3F4QK8 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:04:00 up 57 min,  1 user,  load average: 0.32, 0.23, 0.12

--- nemu/src/monitor/sdb/ftrace.c
+++ nemu/src/monitor/sdb/ftrace.c
@@ -110,7 +110,10 @@ void ftrace_identify_call_ret(int is_jal, int is_jalr, int rd, word_t src1) {
 }
 
 void new_ftrace_log(vaddr_t pc, int call_depth, int is_call, char *func_name, vaddr_t dnpc) {
+  printf("\ncall_depth: %d\n", call_depth);
+  assert(0);
   int len_pc = ((sizeof(vaddr_t) * 8) / 4) + 4; //"0xxxxxxxx: " 12
+  // int len_call_depth = call_depth - 1;
   int len_func_type = 5;                        //"call " or "ret  " 4
   int len_jump_target = len_pc + 2 + strlen(func_name);
   int new_log = len_pc + len_func_type + len_jump_target + 1; //+1 "\n"
@@ -122,7 +125,6 @@ void new_ftrace_log(vaddr_t pc, int call_depth, int is_call, char *func_name, va
   
   sprintf(cur_log_position, FMT_WORD ": ", pc);
   cur_log_position += len_pc;
-  assert(0);
   memset(cur_log_position, ' ', call_depth * 2);
   cur_log_position += call_depth * 2;
   if(is_call) {
